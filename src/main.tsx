e5ef2f7b95ac8e294497b8b752fda9cdacfed875
Tyler Schloesser <tylerschloesser@gmail.com>
2025-12-10 01:26:55 -0800

    fix invariant

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import invariant from 'tiny-invariant'
 import { App } from './App.tsx'
 import './index.css'
+import { invariant } from './utils/invariant.ts'
 
 const container = document.getElementById('root')
 invariant(container)
