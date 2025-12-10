502d2870ced34a8751be57ac56b32dc04fb5a625
Tyler Schloesser <tylerschloesser@gmail.com>
2025-12-09 20:04:34 -0800

    add husky

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
-import App from './App.tsx'
+import { App } from './App.tsx'
 import './index.css'
 
 const container = document.getElementById('root')
