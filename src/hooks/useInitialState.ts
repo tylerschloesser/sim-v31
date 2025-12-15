1f76b6e8e5601ff70ed00fe5350a4e90105557a5
Tyler Schloesser <tylerschloesser@gmail.com>
2025-12-15 13:52:44 +0530

    add initial entity

--- src/hooks/useInitialState.ts
+++ src/hooks/useInitialState.ts
@@ -1,8 +1,21 @@
-import type { AppState } from '../types/state'
+import type { AppState, Entity } from '../types/state'
 
 export function useInitialState(): AppState {
   let nextEntityId = 0
   const entities: AppState['entities'] = {}
+
+  function addEntity(entity: Omit<Entity, 'id'>) {
+    const entityId = `${nextEntityId++}`
+    entities[entityId] = { id: entityId, ...entity }
+  }
+
+  addEntity({
+    x: 2,
+    y: 2,
+    width: 1,
+    height: 1,
+  })
+
   return {
     player: {
       position: { x: 0, y: 0 },
--- src/types/state.ts
+++ src/types/state.ts
@@ -1,17 +1,16 @@
+export interface Entity {
+  id: string
+  x: number
+  y: number
+  width: number
+  height: number
+}
+
 export interface AppState {
   player: {
     position: { x: number; y: number }
   }
   nextEntityId: number
-  entities: Record<
-    string,
-    {
-      id: string
-      x: number
-      y: number
-      width: number
-      height: number
-    }
-  >
+  entities: Record<string, Entity>
   doubleTapDrag: { dx: number; dy: number } | null
 }
