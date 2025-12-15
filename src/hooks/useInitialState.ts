e080582af70380f6d0c7c3a158f9d0df000a519b
Tyler Schloesser <tylerschloesser@gmail.com>
2025-12-15 13:50:27 +0530

    refactor entities

--- src/hooks/useInitialState.ts
+++ src/hooks/useInitialState.ts
@@ -1,11 +1,12 @@
 import type { AppState } from '../types/state'
 
 export function useInitialState(): AppState {
+  const entities: AppState['entities'] = {}
   return {
     player: {
       position: { x: 0, y: 0 },
     },
-    entities: {},
+    entities,
     doubleTapDrag: null,
   }
 }
