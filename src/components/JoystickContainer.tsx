f355f0f7a59e964bc6a832568afdd363473678a3
Tyler Schloesser <tylerschloesser@gmail.com>
2025-12-17 11:43:00 +0530

    update joystick

--- src/components/JoystickContainer.tsx
+++ src/components/JoystickContainer.tsx
@@ -15,6 +15,12 @@ export function JoystickContainer({
         r={32}
         fill="rgba(0, 0, 0, 0.5)"
       />
+      <circle
+        cx={pointer.sx + pointer.dx / 10}
+        cy={pointer.sy + pointer.dy / 10}
+        r={16}
+        fill="rgba(255, 255, 255, 0.5)"
+      />
     </>
   )
 }
