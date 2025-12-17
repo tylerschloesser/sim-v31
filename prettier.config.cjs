aeae1081210ecb6225e5d6f70854bb09b830884f
Tyler Schloesser <tylerschloesser@gmail.com>
2025-12-17 21:21:02 +0700

    fix imports

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-  importOrder: ['^[./]'],
+  importOrder: ['^(?!.*\\.css$)[./]', '^@/', '\\.css$'],
   printWidth: 60,
   plugins: ['@trivago/prettier-plugin-sort-imports'],
 }
