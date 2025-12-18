dabb2261e6079826107a230a881463edb1aa3c56
Tyler Schloesser <tylerschloesser@gmail.com>
2025-12-18 11:02:03 +0700

    add inventory helper

---
+++ src/utils/inventory.ts
@@ -0,0 +1,15 @@
+import type { EntityType, Inventory } from '@/types/state'
+import { invariant } from '@/utils/invariant'
+
+export function inventoryAdd(
+  inventory: Inventory,
+  entityType: EntityType,
+  amount: number = 1,
+): void {
+  // prettier-ignore
+  {
+    invariant(amount > 0, 'Amount to add must be positive')
+    invariant(amount === Math.floor(amount), 'Amount to add must be an integer')
+    inventory[entityType] = (inventory[entityType] ?? 0) + amount
+  }
+}
